ba05733c56a6462a2a69e8f1dc6a5e98ed14f7b6
cheesecake_assassin <santasebastian@yahoo.com>
2022-04-06 21:11:19 -0400

    fixing files

--- app.py
+++ app.py
@@ -1,55 +1,30 @@
-from flask import Flask, render_template, url_for, redirect
-from flask_admin import Admin, BaseView, expose
+from flask import Flask, jsonify, json, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
-from flask_bcrypt import Bcrypt
-from flask_basicauth import BasicAuth
-from flask_admin.contrib.sqla import ModelView
-
-
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-admin = Admin(app, name='microblog', template_mode='bootstrap3')
-basic_auth = BasicAuth(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'thisisasecretkey'
-app.config['BASIC_AUTH_USERNAME'] = 'sasha'
-app.config['BASIC_AUTH_PASSWORD'] = 'sasha'
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-class User(db.Model, UserMixin):
+# CREATE THE SQLALCHEMY USER MODEL
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique = True)
-    password = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(40))
+    home_address = db.Column(db.String(50))
+    email = db.Column(db.String(40))
+    password = db.Column(db.String(40))
+    wishlist = db.relationship(
+        'Book', secondary=wishlist, backref='inWishlist')
 
-class Add(db.Model, UserMixin):
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String(30), nullable = False, unique = True)
-    isbn = db.Column(db.Integer, nullable = False, unique = True)
-    author = db.Column(db.String(20), nullable = False)
-    publisher = db.Column(db.String(20), nullable = False)
-    genre = db.Column(db.String(20), nullable = False)
-    price = db.Column(db.Float, nullable = False)
-    year = db.Column(db.Integer, nullable = False)
-    description = db.Column(db.String(20), nullable = False)
-    rating = db.Column(db.Integer, nullable = False)
-    sold = db.Column(db.Integer, nullable = False)
+    def __init__(self, name, home_address, email, password):
+        self.name = name
+        self.home_address = home_address
+        self.email = email
+        self.password = password
 
-class Cards(db.Model, UserMixin):
+
+# CREATE THE SQLALCHEMY CREDIT CARD MODEL
+class Cards(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable = False)
     card_number = db.Column(db.Integer, nullable = False, unique = True)
@@ -57,83 +32,72 @@ class Cards(db.Model, UserMixin):
     security_code = db.Column(db.Integer, nullable = False)
     zip_code = db.Column(db.Integer, nullable = False)
 
-
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Add, db.session))
-    
-class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder": "Password"})
-
-    submit = SubmitField("Register")
-
-    def validate_username(self, username):
-        existing_user_username = User.query.filter_by(
-            username = username.data).first()
-
-        if existing_user_username:
-            raise ValidationError(
-                "That username already exists. Please choose a different one."
-            )
-
-class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder": "Password"})
-
-    submit = SubmitField("Login")
+    def __init__(self, name, card_number, expiration_date, security_code, zip_code):
+        self.name = name
+        self.card_number = card_number
+        self.expiration_date = expiration_date
+        self.security_code = security_code
+        self.zip_code = zip_code
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+# FUNCTION TO RETURN A USER DICTIONARY
+def user_dict(new_user):
+    user = {
+        "id": new_user.id,
+        "name": new_user.name,
+        "home_address": new_user.home_address,
+        "email": new_user.email,
+        "password": new_user.password
+    }
+    return user
 
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('admin.index'))
-        else:
-            return redirect (url_for('incorrect'))
-            return render_template ('incorrect.html')
-    return render_template('login.html', form = form)
 
-@app.route('/incorrect', methods = ['GET', 'POST'])
-def incorrect():
-    return render_template ('incorrect.html')
+# FUNCTION TO RETURN A CREDIT CARD DICTIONARY
+def card_dict(new_card):
+    card = {
+        "id": new_card.id,
+        "name": new_card.name,
+        "card_number": new_card.card_number,
+        "expiration_date": new_card.expirtation_date,
+        "security_code": new_card.security_code,
+        "zip_code": new_card.zip_code
+    }
+    return card
 
-@app.route('/register', methods = ['GET', 'POST'])
-def register():
-    form = RegisterForm()
 
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
+# CREATE A USER
+@app.route('/user', methods=['POST'])
+def user():
 
-    return render_template('register.html', form = form)
+    # Received posted data and store it in variables
+    name = request.json['name']
+    home_address = request.json['home_address']
+    email = request.json['email']
+    password = request.json['password']
+    print("this is password" + password)
+    # Create a new user
+    new_user = User(name, home_address, email, password)
 
-@app.route('/dashboard', methods = ['GET', 'POST'])
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
+    # Add a user to the database
+    db.session.add(new_user)
+    db.session.commit()
 
-@app.route('/logout', methods = ['GET', 'POST'])
-@login_required
-def logout():
-    logout_user()
-    return render_template('dashboard.html')
+    # Store all the user data in a variable and return all that data
+    user = user_dict(new_user)
+    return json.dumps(user)
+
+
+# GET A USER BY USERNAME
+@app.route('/user', methods=['POST'])
+def get_user_by_name():
+    name = request.json['name']
+    users = User.query.filter_by(name).all()
+    userList = []
+    for user in users:
+        all_users = user_dict(user)
+        userList.append(all_users)
+    result = json.dumps(userList)
+    return result
 
 if __name__ == "__main__":
     app.run(debug = True)
