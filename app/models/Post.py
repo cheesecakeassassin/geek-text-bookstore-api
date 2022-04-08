537cdf5d0e45a2a80393b517b9c0586146e51758
cheesecake_assassin <santasebastian@yahoo.com>
2022-04-08 00:22:13 -0400

    adding temp seeds to post

--- app/models/Post.py
+++ app/models/Post.py
@@ -2,3 +2,14 @@ from datetime import datetime
 from app.db import Base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+
+class Post(Base):
+  __tablename__ = 'posts'
+  id = Column(Integer, primary_key=True)
+  title = Column(String(100), nullable=False)
+  post_url = Column(String(100), nullable=False)
+  user_id = Column(Integer, ForeignKey('users.id'))
+  created_at = Column(DateTime, default=datetime.now)
+  updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+  user = relationship('User')
--- seeds.py
+++ seeds.py
@@ -1,5 +1,6 @@
 from app.models import User
 from app.db import Session, Base, engine
+from app.models import User, Post
 
 # drop and rebuild tables
 Base.metadata.drop_all(engine)
@@ -14,6 +15,17 @@ db.add_all([
   User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
   User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
   User(username='djiri4', email='gmidgley4@weather.com', password='password123')
+])
+
+db.commit()
+
+# insert posts
+db.add_all([
+  Post(title='Donec posuere metus vitae ipsum', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
+  Post(title='Morbi non quam nec dui luctus rutrum', post_url='https://nasa.gov/donec.json', user_id=1),
+  Post(title='Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue', post_url='https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx', user_id=2),
+  Post(title='Nunc purus', post_url='http://desdev.cn/enim/blandit/mi.jpg', user_id=3),
+  Post(title='Pellentesque eget nunc', post_url='http://google.ca/nam/nulla/integer.aspx', user_id=4)
 ])
 
 db.commit()
