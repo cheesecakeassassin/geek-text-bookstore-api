132aa342c0e2e41c7ed492fd40ef0fd750ce2e48
cheesecake_assassin <santasebastian@yahoo.com>
2022-04-08 00:12:58 -0400

    adding validation for email and password with sqlalchemy orm validates

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,6 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
 
 class User(Base):
   __tablename__ = 'users'
@@ -7,3 +8,16 @@ class User(Base):
   username = Column(String(50), nullable=False)
   email = Column(String(50), nullable=False, unique=True)
   password = Column(String(100), nullable=False)
+
+  @validates('email')
+  def validate_email(self, key, email):
+    # Make sure email address contains @ character
+    assert '@' in email
+
+    return email
+
+  @validates('password')
+  def validate_password(self, key, password):
+    assert len(password) > 4
+
+    return password
---
+++ seeds.py
@@ -0,0 +1,21 @@
+from app.models import User
+from app.db import Session, Base, engine
+
+# drop and rebuild tables
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+db = Session()
+
+# insert users
+db.add_all([
+  User(username='alesmonde0', email='nwestnedge0@cbc.ca', password='password123'),
+  User(username='jwilloughway1', email='rmebes1@sogou.com', password='password123'),
+  User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
+  User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
+  User(username='djiri4', email='gmidgley4@weather.com', password='password123')
+])
+
+db.commit()
+
+db.close()
