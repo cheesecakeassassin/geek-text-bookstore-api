73eb390d255a7cdf5b82e484a3fd164326df69f7
cheesecake_assassin <santasebastian@yahoo.com>
2022-04-09 07:56:01 -0400

    adding orm and filters

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.db import init_db
 from app.routes import home
 from app.routes import home, dashboard
+from app.utils import filters
 
 def create_app(test_config=None):
   # Set up app config
@@ -10,6 +11,9 @@ def create_app(test_config=None):
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
 
   @app.route('/hello')
   def hello():
